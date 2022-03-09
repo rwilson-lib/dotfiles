a3ca8ba31b9f8b2f5378056c1b6daf1bafe0bf48
Ramus Jabee Lloyd Wilson <ramuswilson@gmail.com>
2022-03-09 01:27:38 +0000

    include a custom emacs config file

---
+++ .emacs.d/custom.el
@@ -0,0 +1,24 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(custom-safe-themes
+   '("c64a5bed92a86a67e5a916688ffaa53b99a34422884cf6acac0fe151e39a7e3f" "87a176f4b715c755444f45361ff877081191f9e0e10e1ec4da655be901c7dbaf" "3263bd17a7299449e6ffe118f0a14b92373763c4ccb140f4a30c182a85516d7f" "36ca8f60565af20ef4f30783aa16a26d96c02df7b4e54e9900a5138fb33808da" "021321ae56a45794f43b41de09fb2bfca184e196666b7d7ff59ea97ec2114559" "2d035eb93f92384d11f18ed00930e5cc9964281915689fa035719cab71766a15" "edf1f9e74600cac84368d8c1ae2158db85217c3a02e3b1470545462a64cea016" "6df412e59dbfe7f72f24319b9ee4513e40bb0e44384fc93a2c77399e641348f6" "dc11cee30927281fe3f5c77372119d639e77e86aa794dce2a6ff019afdfbec9e" default))
+ '(global-display-line-numbers-mode t)
+ '(menu-bar-mode nil)
+ '(package-selected-packages
+   '(window company-tabnine cape corfu hl-block-mode hl-prog-extra all-the-icons-completion avy-embark-collect json-mode ob-async mu4e-alert langtool lsp-pyright plantuml-mode speed-type js-react-redux-yasnippets company-web eziam-theme yaml-mode python-insert-docstring org-anki org-drill org-projectile todotxt-mode magit-todos hl-todo company-restclient ob-restclient restclient org-noter-pdftools org-remark async-await ivy vuiet lsp-ltex lsp-latex scanner evil-org flycheck-grammarly auto-yasnippet popup coterm key-quiz async the-matrix-theme company-lsp exotica-theme prettier-js rjsx-mode link-hint ipython-shell-send password-store-otp password-store org-tree-slide hide-mode-line format-all web-mode smartparens rainbow-mode live-py-mode python-environment fd-dired consult-fd term-mode leuven-theme mu4e-dashboard org-present django-html-mode tao-theme jazz-theme org-crypt epa-file ledger-mode ledger try org-roam-ui org-roam eink-theme evil-tree-edit tree-edit crdt projectile-ag ag projectile-ripgrep ripgrep synosaurus flyspell-lazy evil-vimish-fold vimish-fold csv-mode aweshell evil-multiedit treemacs-evil treemacs-all-the-icons embark-consult consult-company consult-projectile flymake-python-pyflakes consult-yasnippet js-mode prog-mode consult-flycheck consult-lsp avy-flycheck sqlformat edbi-sqlite javascript-mode pretty-symbols-mode composite embark consult marginalia orderless vertico multi-vterm multi-term display-battery-mode pipenv pyvenv git-timemachine git-gutter emmet-mode treemacs-persp keycast treemacs-magit treemacs-icons-dired treemacs-projectile ob-napkin evil-surround evil-matchit exec-path-from-shell latex-preview-pane org-msg org-alert ox-reveal mu4e-marker-icons undo-fu-session undo-fu undo-tree org-mu4e mu4e-org org-mime org-notmuch mu4e company-yasnippet yasnippet-snippets yasnippet sql-indent sql-upcase sql-mode winum company-box visual-fill-column org-bullets dap-mode which-key vterm use-package typescript-mode rainbow-delimiters python-mode lsp-ui lsp-treemacs helpful general forge evil-collection eterm-256color doom-themes doom-modeline))
+ '(pdf-tools-handle-upgrades nil)
+ '(send-mail-function 'mailclient-send-it)
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 25)
+ '(tool-bar-mode nil)
+ '(warning-suppress-types '((use-package) (comp) (comp) (comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inhert ace-jump-face-background :height 1.5)))))
